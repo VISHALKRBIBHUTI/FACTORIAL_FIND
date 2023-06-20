cb35c2d95df3ddef0a8c262452c0d5cdb9ba2b1c
VISHAL KUMAR BIBHUTI <126374785+VISHALKRBIBHUTI@users.noreply.github.com>
2023-06-20 10:09:57 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int is_factorial(int num){
+    int fact=1;
+    for(int i=num;i>=1;i--){
+        fact=fact*i;
+    }
+    return fact;
+}
+int main(){
+    int num1;
+    cin>>num1;
+    cout<<"enter the vnumber you want to check for it's factorial "<<endl;
+    cout<<"factorial of number "<<num1<<"is   "<<  is_factorial(num1);
+    return 0;
+
+}
